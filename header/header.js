0d01b475ec92214da5f3eae77e9ab94ac5789b6e
hellen <hellenyct@mail.com>
2024-07-11 12:18:38 -0600

    v 0.2

---
+++ header/header.js
@@ -0,0 +1,4 @@
+let header_DOM = document.querySelector(".header")
+
+
+header_DOM.innerHTML = '<h1 class= "titulo">@Vanner</h1> <a href= "https://github.com/Vane1611gt/examen2.git" class="btn-github">Github</a>';
